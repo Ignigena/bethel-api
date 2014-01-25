e6f4a34f8bb032f7920ca102f3821bdfce3140a1
Albert Martin <albertmartin@mac.com>
2014-01-25 17:29:41 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,34 @@
+require('newrelic');
+
+var restify = require('restify');
+var podcast = require('./commands/podcast');
+
+var ip_addr = '127.0.0.1';
+var port = '8080';
+
+var server = restify.createServer({
+    name: 'BethelApi'
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+server.use(restify.gzipResponse());
+
+server.get('/', welcome);
+
+var PODCAST = '/podcast';
+server.get({path: PODCAST, version: '0.0.1'}, podcast.showAllPodcastMedia);
+server.get({path: PODCAST + '/:podcastId', version: '0.0.1'}, podcast.findAssociatedPodcastMedia);
+server.get({path: PODCAST + '/:podcastId/:mediaId', version: '0.0.1'}, podcast.findPodcastMedia);
+server.post({path: PODCAST, version: '0.0.1'}, podcast.postNewPodcastMedia);
+server.post({path: PODCAST + '/:podcastId/:mediaId/edit', version: '0.0.1'}, podcast.editPodcastMedia);
+server.del({path: PODCAST + '/:podcastId/:mediaId', version: '0.0.1'}, podcast.deletePodcastMedia);
+
+server.listen(port, ip_addr, function () {
+    console.log('%s listening at %s ', server.name, server.url);
+});
+
+function welcome(req, res, next) {
+    res.send('Bethel API');
+}
---
+++ commands/podcast.js
@@ -0,0 +1,78 @@
+var mongojs = require('mongojs');
+var db = mongojs('127.0.0.1:27017/podcast', ['podcast']);
+var media = db.collection('media');
+
+function setHeaders(res) {
+    res.cache('_public_', {maxAge: 600});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Vary', 'Accept-Encoding');
+}
+
+module.exports = {
+    showAllPodcastMedia: function (req, res, next) {
+        setHeaders(res);
+
+        media.find().sort({date: -1}, function (err, success) {
+            if (success) {
+                res.send(200, success);
+                return next();
+            }
+            return next(err);
+        });
+    },
+    findAssociatedPodcastMedia: function (req, res, next) {
+        setHeaders(res);
+
+        media.find({podcast: Number(req.params.podcastId)}, function (err, success) {
+            if (success) {
+                res.send(200, success);
+                return next();
+            }
+            return next(err);
+        });
+    },
+    findPodcastMedia: function (req, res, next) {
+        setHeaders(res);
+
+        media.find({podcast: Number(req.params.podcastId), uuid: req.params.mediaId}, function (err, success) {
+            if (success) {
+                res.send(200, success);
+                return next();
+            }
+            return next(err);
+        });
+    },
+    postNewPodcastMedia: function (req, res, next) {
+        var newMedia = {};
+        newMedia.podcast = req.params.podcastId;
+        newMedia.uuid = req.params.mediaId;
+        newMedia.title = req.params.title;
+        newMedia.date = new Date(req.params.date);
+        newMedia.description = req.params.description;
+        newMedia.duration = req.params.duration;
+
+        setHeaders(res);
+
+        media.save(newMedia, function (err, success) {
+            if (success) {
+                res.send(201, newMedia);
+                return next();
+            }
+            return next(err);
+        });
+    },
+    editPodcastMedia: function (req, res, next) {
+        //@todo Implement.
+    },
+    deletePodcastMedia: function (req, res, next) {
+        setHeaders(res);
+
+        media.remove({_id: mongojs.ObjectId(req.params.jobId)}, function (err, success) {
+            if (success) {
+                res.send(204);
+                return next();
+            }
+            return next(err);
+        })
+    }
+};
